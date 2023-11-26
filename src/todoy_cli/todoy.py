aa8758db078c3d7ca97e576b0672644027b2fbd1
espehon <gregg.howlett@gmail.com>
2023-11-26 07:42:50 -0800

    Fixed add_argument settings

--- src/todoy_cli/todoy.py
+++ src/todoy_cli/todoy.py
@@ -7,16 +7,16 @@ parser = argparse.ArgumentParser()
 
 # TODO: fix actions and nargs. All arguments can technically be passed 1 or more task integers (except --edit)
 parser.add_argument('-t', '--task', action='store_true', help='Add a new task')
-parser.add_argument('-c', '--complete', nargs=1, action='store_true', help='Mark task(s) complete')
-parser.add_argument('-s', '--switch', nargs=1, action='store_true', help='Toggle task(s) as started/stopped')
-parser.add_argument('-f', '--flag', nargs=1, action='store_true', help='Flag task(s) with astrict (*)')
-parser.add_argument('-p', '--priority', nargs=2, action='store_true', help='Set the priority of task [T] to [P]')
-parser.add_argument('-e', '--edit', nargs=1, action='store_true', help='Enter edit mode on a task')
-parser.add_argument('-d', '--delete', nargs=1, action='store_true', help='Mark task [T] for deletion')
+parser.add_argument('-c', '--complete', nargs='+', action='store', type=int, help='Mark task(s) complete')
+parser.add_argument('-s', '--switch', nargs='+', action='store', type=int, help='Toggle task(s) as started/stopped')
+parser.add_argument('-f', '--flag', nargs='+', action='store', type=int, help='Flag task(s) with astrict (*)')
+parser.add_argument('-p', '--priority', nargs=2, action='store', type=int, help='Set the priority of task [T] to [P]')
+parser.add_argument('-e', '--edit', nargs=1, action='store', type=int, help='Enter edit mode on a task')
+parser.add_argument('-d', '--delete', nargs='+', action='store', type=int, help='Mark task [T] for deletion')
 parser.add_argument('--clear', action='store_true', help='Delete all resolved tasks and renumber')
 parser.add_argument('text', nargs=argparse.REMAINDER, help='Task description')
 
-args = parser.parse_known_args(['-t', 'hello', 'world!'])
+args = parser.parse_known_args(['--clear', '1', 'hello'])
 
 print(args)
 
@@ -30,3 +30,5 @@ data = {1:{
     'priority': 0, # 0,1,2,3
     'flag': False
 }}
+
+print("<<< EOF >>>")
