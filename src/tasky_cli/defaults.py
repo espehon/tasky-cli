0fc7e74bcf68acb7f7f10627b2e033f2ab5d77ad
espehon <gregg.howlett@gmail.com>
2024-01-23 20:02:01 -0800

    added stdout check and dynamic configs

--- src/tasky_cli/defaults.py
+++ src/tasky_cli/defaults.py
@@ -3,7 +3,14 @@
 
 import sys
 
-defaultCharacters = {
+
+if sys.stdout.encoding.lower() == 'utf-8':
+    style = 'fancy'
+else:
+    style = 'plain'
+
+
+DEFAULT_CHARS = {
     'newTaskSymbol': {
         'fancy': '[!]',
         'plain': '[!]'
@@ -46,3 +53,32 @@ defaultCharacters = {
     }
 }
 
+
+default_configs = f"""\
+[Settings]
+taskPath = "~/.local/share/tasky/"
+taskFile = "tasky.json"
+
+newTaskSymbol = "{DEFAULT_CHARS['newTaskSymbol'][style]}"
+startedTaskSymbol = "{DEFAULT_CHARS['startedTaskSymbol'][style]}"
+stoppedTaskSymbol = "{DEFAULT_CHARS['stoppedTaskSymbol'][style]}"
+completeTaskSymbol = "{DEFAULT_CHARS['completeTaskSymbol'][style]}"
+flagSymbol = "{DEFAULT_CHARS['flagSymbol'][style]}"
+flagSymbolAlt = "{DEFAULT_CHARS['flagSymbolAlt'][style]}"
+
+boarderColor = "bright_black"
+newTaskColor = "red"
+startedTaskColor = "bright_yellow"
+stoppedTaskColor = "bright_red"
+completeTaskColor = "bright_green"
+
+priorityColor1 = "white"
+priorityColor2 = "cyan"
+priorityColor3 = "yellow"
+priorityColor4 = "red"
+
+prioritySymbol1 = "{DEFAULT_CHARS['prioritySymbol1'][style]}"
+prioritySymbol2 = "{DEFAULT_CHARS['prioritySymbol2'][style]}"
+prioritySymbol3 = "{DEFAULT_CHARS['prioritySymbol3'][style]}"
+prioritySymbol4 = "{DEFAULT_CHARS['prioritySymbol4'][style]}"
+"""
